72c4e391a5d1897064e32ea3183ded4e4670e431
EC2 Default User <ec2-user@ip-172-31-43-144.ec2.internal>
2022-08-11 03:36:01 +0000

    実装終了

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -3,27 +3,16 @@ class Admin::OrderDetailsController < ApplicationController
   before_action :authenticate_admin!
   
   def update
-    @order_detail = OrderDetail.find(params[:id])
-    # @order = @order_detail.order
-    @order_detail.update(order_detail_params)
-   
+    @order_detail = OrderDetail.find(params[:id]) #注文商品の特定
+    @order = @order_detail.order  #注文商品から注文テーブルの呼び出し
+    @order_detail.update(order_detail_params)  #製作ステータスの更新
     
-    
-   
-    # #byebug
-    # @order_details = @order.order_details
-   
-
-    # if @order_details.where(production_status: "製作中").count >= 1
-    #   @order.status = "製作中"
-    #   @order.save
-    # end
-
-    # if @order.order_details.count == @order_details.where(production_status: "製作完了").count
-    #   @order.status = "発送準備中"
-    #   @order.save
-    # end
-     redirect_to admin_order_path(@order_detail.order.id), notice: "制作ステータスを更新しました"
+		if @order_detail.production_status == "in_production" #製作ステータスが「製作中」なら
+			@order.update(status: "in_production") #注文ステータスを「製作中」　に更新
+		elsif @order.order_details.count ==  @order.order_details.where(production_status: "production_complete").count #注文した商品の個数と製作完了した商品の個数が一致した場合
+		  @order.update(status: "preparing_delivery") #注文ステータスを「発送準備中]に更新
+		end
+      redirect_to admin_order_path(@order_detail.order.id), notice: "制作ステータスを更新しました"
   end
   
   private
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -14,16 +14,13 @@ class Admin::OrdersController < ApplicationController
   end
   
   def update
-    @order = Order.find(params[:id]) 
-    @order.update(order_params)
-    @order_details = @order.order_details
-    
-    # if @order.status == "入金確認"
-    #   @order_details.each do |order_detail|
-    #     order_detail.production_status = "製作待ち"
-    #     order_detail.save
-    #   end
-    # end
+    @order = Order.find(params[:id])  #注文詳細の特定
+    @order.update(order_params) #注文ステータスの更新
+    @order_details = @order.order_details #注文から紐付く注文した商品の取得
+
+ 	 if @order.status == "payment_confirmation" #注文ステータスが「入金確認」なら
+	    @order_details.update_all(production_status: "production_pending") #製作ステータスを「製作待ち」に更新
+	 end
     redirect_to admin_order_path(@order.id), notice: "注文ステータスを更新しました"
   end
   
